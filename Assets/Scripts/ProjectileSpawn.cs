16887327698fe4e3472b76f9b49dae32e04a8cc3
CartiFanatic <deykrishnav@gmail.com>
2024-04-18 11:26:38 -0400

    k megh

--- Assets/Scripts/ProjectileSpawn.cs
+++ Assets/Scripts/ProjectileSpawn.cs
@@ -12,7 +12,6 @@ public class ProjectileSpawn : MonoBehaviour
 {
     [SerializeField] GameObject[] projectilePrefab;
     [SerializeField] GameObject projectileSpawn;
-    [SerializeField] Dropdown projectileSelector;
     [SerializeField] Button start;
     int projectileChosen;
 
