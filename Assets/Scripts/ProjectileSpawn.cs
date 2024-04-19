66c105c117c2eb327a119e27be5dba1d5704013c
CartiFanatic <deykrishnav@gmail.com>
2024-04-19 11:04:53 -0400

    trying yto fix

--- Assets/Scripts/ProjectileSpawn.cs
+++ Assets/Scripts/ProjectileSpawn.cs
@@ -7,21 +7,16 @@ using UnityEditor;
 using UnityEditor.Experimental.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class ProjectileSpawn : MonoBehaviour
 {
-    [SerializeField] GameObject[] projectilePrefab;
-    [SerializeField] GameObject projectileSpawn;
-    [SerializeField] Button start;
-    int projectileChosen;
+
+    [SerializeField] TMP_Dropdown projectileDropdown;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Button btn = start.GetComponent<Button>();
-        btn.onClick.AddListener(TaskOnClick);
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
@@ -29,48 +24,5 @@ public class ProjectileSpawn : MonoBehaviour
     }
 
 
-    public void projectilePick()
-    {
-        if (projectileSelector.value==1)
-        {
-            projectileChosen = 1;
-        }
-
-        if (projectileSelector.value == 2)
-        {
-            projectileChosen = 2;
-        }
-
-        if (projectileSelector.value == 3)
-        {
-            projectileChosen = 3;
-        }
-
-
-
-
-    }
-
-    void TaskOnClick()
-    {
-        if (projectileChosen == 1)
-        {
-            Instantiate(projectilePrefab[1], projectileSpawn.transform.position, Quaternion.identity);
-        }
-
-        if (projectileChosen == 2)
-        {
-            Instantiate(projectilePrefab[2], projectileSpawn.transform.position, Quaternion.identity);
-        }
-
-        if (projectileChosen == 3)
-        {
-            Instantiate(projectilePrefab[3], projectileSpawn.transform.position, Quaternion.identity);
-        }
-
-
-
-    }
-
-
+   
 }
